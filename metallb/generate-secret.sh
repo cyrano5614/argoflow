7bd1b682478808c2927a27a88727d2df265d6ea5
Inhan Song <cyrano5614@prost.home>
2021-05-10 05:12:11 -0400

    metallb secret generate with sealed secret

--- metallb/generate-secret.sh
+++ metallb/generate-secret.sh
@@ -3,17 +3,8 @@
 # Using bitnami's sealed secrets to generate secret for argocd dex connectors
 # pending https://github.com/argoproj/argo-cd/issues/3685 to be resolved
 
-CLIENTID=$1
-CLIENTSECRETS=$2
-CLIENTCALLBACK=$3
-NAMESPACE="${NAMESPACE:-argocd}"
-SECRETNAME="${SECRETNAME:-argocd-user-github-secret}"
+NAMESPACE="${NAMESPACE:-metallb-system}"
+SECRETNAME="${SECRETNAME:-memberlist}"
 
-if [[ -z "$CLIENTID" ]] || [[ -z "$CLIENTSECRETS" ]] || [[ -z "$CLIENTCALLBACK" ]]; then
-    echo "Must provide CLIENTID, CLIENTSECRETS, CLIENTCALLBACK" 1>&2
-    exit 1
-fi
-
-echo -n bar | kubectl -n $NAMESPACE create secret generic $SECRETNAME --dry-run=client --from-literal=clientid=$CLIENTID --from-literal=clientsecrets=$CLIENTSECRETS --from-literal=clientcallback=$CLIENTCALLBACK -o json > exposed-secrets.json
-
+echo -n bar | kubectl -n $NAMESPACE create secret generic $SECRETNAME --dry-run=client --from-literal=secretkey="$(openssl rand -base64 128)" -o json > exposed-secrets.json
 kubeseal --controller-name=sealed-secrets --controller-namespace=sealed-secrets <exposed-secrets.json >sealed-secrets.json
